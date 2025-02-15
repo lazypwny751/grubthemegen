75e7001d35453b261cd82fefd2a9adc95efa4cea
lazypwny751 <testfordnshijacking@gmail.com>
2025-02-15 22:17:19 +0300

    image, label components

--- lib/grubtheme.rb
+++ lib/grubtheme.rb
@@ -10,18 +10,19 @@
 
 class GrubTheme
 	def initialize(
-		title_text			= nil,
-		title_font			= nil,
-		title_color			= nil,
-		message_font		= nil,
-		message_color		= nil,
-		message_bg_color	= nil,
-		desktop_image 		= nil,
-		desktop_color 		= nil,
-		terminal_box 		= nil,
-		terminal_font 		= nil
+		title_text: 		nil,
+		title_font:			nil,
+		title_color: 		nil,
+		message_font: 		nil,
+		message_color: 		nil,
+		message_bg_color:	nil,
+		desktop_image:		nil,
+		desktop_color:		nil,
+		terminal_box:		nil,
+		terminal_font:		nil
 	)
 		@buf				= ""
+		@bufcomp			= ""
 
 		@title_text 		= title_text
 		@title_font 		= title_font
@@ -98,65 +99,116 @@ class GrubTheme
 		@terminal_font = terminal_font
 	end
 
-	# Generate.
-	def gen()
-		if checkup()
-			unless @title_text.nil?
-				push("title-text: \"#{@title_text}\"")
-			end
+	# Components.
+	## Label.
+	def label(text: nil, font: nil, color: nil, align: nil, visible: nil, **kwargs)
+		# id is required for generation.
+		unless kwargs[:id].nil?
+			buf = "+ label {\n"
+			# text, font, color, align, visible
+			buf += "\ttext = \"#{text}\"\n" # buf += write2buf(text, "text", true)
+			buf += common_properties(**kwargs)
+			buf += "}\n"
 
-			unless @title_font.nil?
-				push("title-font: \"#{@title_font}\"")
-			end
-
-			unless @title_color.nil?
-				push("title-color: \"#{@title_color}\"")
-			end
-
-			unless @message_font.nil?
-				push("message-font: \"#{@message_font}\"")
-			end
-
-			unless @message_color.nil?
-				push("message-color: \"#{@message_color}\"")
-			end
-
-			unless @message_bg_color.nil?
-				push("message-bg-color: \"#{@message_bg_color}\"")
-			end
-
-			unless @desktop_image.nil?
-				push("desktop-image: \"#{@desktop_image}\"")
-			end
-
-			unless @desktop_color.nil?
-				push("desktop-color: \"#{@desktop_color}\"")
-			end
-
-			unless @terminal_box.nil?
-				push("terminal-box: \"#{@terminal_box}\"")
-			end
-
-			unless @terminal_font.nil?
-				push("terminal-font: \"#{@terminal_font}\"")
-			end
-
-			@buf
+			@bufcomp += buf
 		else
-			raise("Required fields not yet met.")
+			warn("\"id\" is required for this component.", uplevel: 1)
 		end
 	end
 
-	## This checkup is just for only required fields.
-	private def checkup()
-		ret = true
+	## Image.
+	def image(file: nil, **kwargs)
+		unless file.nil?
+			buf = "+ image {\n"
+			buf += "\tfile = \"#{file}\"\n"
+			buf += common_properties(**kwargs)
+			buf += "}\n"
 
-		if @title_text.nil?
-			warn("title_text is required.", uplevel: 2)
-			ret = false
+			@bufcomp += buf
+		else
+			warn("\"file\" is required for this component.", uplevel: 1)
+		end
+	end
+
+	## Progress bar.
+
+	## Circular progress.
+
+	## Boot menu.
+
+	# Generate.
+	def gen()
+		unless @title_text.nil?
+			push("title-text: \"#{@title_text}\"")
 		end
 
-		ret
+		unless @title_font.nil?
+			push("title-font: \"#{@title_font}\"")
+		end
+
+		unless @title_color.nil?
+			push("title-color: \"#{@title_color}\"")
+		end
+
+		unless @message_font.nil?
+			push("message-font: \"#{@message_font}\"")
+		end
+
+		unless @message_color.nil?
+			push("message-color: \"#{@message_color}\"")
+		end
+
+		unless @message_bg_color.nil?
+			push("message-bg-color: \"#{@message_bg_color}\"")
+		end
+
+		unless @desktop_image.nil?
+			push("desktop-image: \"#{@desktop_image}\"")
+		end
+
+		unless @desktop_color.nil?
+			push("desktop-color: \"#{@desktop_color}\"")
+		end
+
+		unless @terminal_box.nil?
+			push("terminal-box: \"#{@terminal_box}\"")
+		end
+
+		unless @terminal_font.nil?
+			push("terminal-font: \"#{@terminal_font}\"")
+		end
+
+		if @bufcomp != ""
+			@buf += @bufcomp
+		end
+
+		@buf
+	end
+
+	private def common_properties(**kwargs)
+		# left, top, width, height, id
+		buf = ""
+		unless (kwargs[:left]).nil?
+			buf += "\tleft = #{kwargs[:left]}\n"
+		end
+
+		unless (kwargs[:top]).nil?
+			buf += "\ttop = #{kwargs[:top]}\n"
+		end
+
+		unless (kwargs[:width]).nil?
+			buf += "\twidth = #{kwargs[:width]}\n"
+		end
+
+		unless (kwargs[:height]).nil?
+			buf += "\theight = #{kwargs[:height]}\n"
+		end
+
+		unless (kwargs[:id]).nil?
+			buf += "\tid = \"#{kwargs[:id]}\"\n"
+		end
+
+		buf
 	end
 
 	private def push(data)
--- test/theme-test.rb
+++ test/theme-test.rb
@@ -10,4 +10,8 @@ theme.message_bg_color = "#000000"
 theme.desktop_color = "#2857a8"
 theme.terminal_font = "Ubuntu Regular"
 
+theme.label(id: "__timeout__")
+theme.image(file: "merhaba.png", id: "merhaba")
+theme.image(file: "deneme.png")
+
 puts(theme.gen())
